9bd60b37e8c02d0f011f37876d378bd67b86b71e
vanessaashrie <vanessaashriee@gmail.com>
2026-06-11 16:04:37 +0530

    AWS integration complete

--- backend/main.py
+++ backend/main.py
@@ -1,37 +1,19 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from openai import OpenAI
-from dotenv import load_dotenv
-from fastapi.middleware.cors import CORSMiddleware
-import os
-
-load_dotenv()
+import boto3
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # allows React to connect
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+dynamodb = boto3.resource(
+    "dynamodb",
+    region_name="eu-north-1"
 )
 
-client = OpenAI(
-    api_key=os.getenv("OPENAI_API_KEY")
-)
+table = dynamodb.Table("AmazeOnUsers")
 
-class PromptRequest(BaseModel):
-    prompt: str
+@app.get("/user/{user_id}")
+def get_user(user_id: str):
+    response = table.get_item(
+        Key={"userId": user_id}
+    )
 
-
-@app.get("/")
-def root():
-    return {"message": "Hello Hackathon"}
-
-
-@app.post("/ask")
-def ask(request: PromptRequest):
-    return {
-        "answer": f"Mock AI response: {request.prompt}"
-    }
+    return response.get("Item", {})
