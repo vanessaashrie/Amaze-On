4bcf846df99c9b919f1e12a0694cacaf10dc1e34
vanessaashrie <vanessaashriee@gmail.com>
2026-06-11 16:15:58 +0530

    Integrate DynamoDB with FastAPI

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
 import boto3
+
+load_dotenv()
+REGION = os.getenv("AWS_REGION")
+TABLE_NAME = os.getenv("DYNAMODB_TABLE")
+BUCKET = os.getenv("S3_BUCKET")
 
 app = FastAPI()
 
@@ -13,7 +20,10 @@ table = dynamodb.Table("AmazeOnUsers")
 @app.get("/user/{user_id}")
 def get_user(user_id: str):
     response = table.get_item(
-        Key={"userId": user_id}
+        Key={
+            "userId": user_id
+        }
     )
 
     return response.get("Item", {})
+
